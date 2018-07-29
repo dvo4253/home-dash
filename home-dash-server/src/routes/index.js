cc0b04e58917cb502898d97e8f72029e77276ae1
Nathan Dvorachek <dvo4253.miscaccounts@gmail.com>
2018-07-28 22:25:57 -0500

    added health and fallback routes

--- home-dash-server/src/routes/index.js
+++ home-dash-server/src/routes/index.js
@@ -2,10 +2,13 @@ import express from 'express';
 
 import root from './root';
 import nestRoutes from './nest';
+import healthRoute from './health';
+import fallbackRoute from './fallback';
 
 let apiRouter = express.Router();
-
-apiRouter.use('/', root);
+apiRouter.use('/health', healthRoute);
 apiRouter.use('/nest', nestRoutes);
+apiRouter.use('/', root);
+apiRouter.use('*', fallbackRoute);
 
 export default apiRouter;
